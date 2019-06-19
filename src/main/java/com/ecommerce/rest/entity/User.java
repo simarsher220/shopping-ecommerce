895bf5136cfef22dfd2ee36677acea35c89ab10b
Simar Singh <simar@codejudge.io>
2019-06-19 14:16:26 +0530

    change table name

--- src/main/java/com/ecommerce/rest/entity/User.java
+++ src/main/java/com/ecommerce/rest/entity/User.java
@@ -10,7 +10,7 @@ import java.util.Set;
 import java.util.UUID;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class User {
 
